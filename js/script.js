c4253e848e858be7797a6a8d8b9117defef5bbf2
sermark <win295@gmail.com>
2017-06-11 17:09:45 +0300

    correct mistake with turns

--- js/script.js
+++ js/script.js
@@ -14,6 +14,10 @@ let items;
 let turns = 0;
 
 let	fieldGame = document.getElementById('container');
+let overlay = document.querySelector('.overlay');
+let popup = document.querySelector('.popup');
+let head = popup.querySelector('.head');
+let close = popup.querySelector('.close');
 let buttonReset = document.createElement('button');
 buttonReset.classList.add('button');
 buttonReset.textContent = 'Reset Game';
@@ -32,47 +36,52 @@ buttonReset.onclick = () => refresh (items);
 
 items.forEach( (e) => {
 	e.addEventListener('click', () => {
-		if (turns == 9) {
-			alert('Tie game');
-		} else if (e.classList.contains('disable')) {
-			alert('This spot is already filled');
+		if (e.classList.contains('disable')) {
+			popupShow ('This spot is already filled');
 		} else if (turns%2 == 0){
 			turns++;
 			e.textContent = 'o';
 			e.classList.add('disable', 'red');
 			if  (items[0].textContent == 'o' && items[1].textContent == 'o' && items[2].textContent == 'o'){
 					cross.classList.add('row1');
-					alert('winner o');
 					setTimeout(function () {refresh(items);}, 2000);
-			} 
+					popupShow ('Winner: O');
+				} 
 		   	   	else if(items[3].textContent == 'o' && items[4].textContent == 'o' && items[5].textContent == 'o'){
 		   	   		cross.classList.add('row2');
-					alert('winner o');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: O');
 		   	   	} 
 		   	   	else if (items[6].textContent == 'o' && items[7].textContent == 'o' && items[8].textContent == 'o'){
 		   	   		cross.classList.add('row3');
-					alert('winner o');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: O');
 		   	   	} 
 		   	   	else if (items[0].textContent == 'o' && items[3].textContent == 'o' && items[6].textContent == 'o'){
 		   	   		cross.classList.add('col1');
-					alert('winner o');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: O');
 		   	   	} 
 		       	else if (items[1].textContent == 'o' && items[4].textContent == 'o' && items[7].textContent == 'o'){
 		       		cross.classList.add('col2');
-					alert('winner o');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: O');
 		       	} 
 		   	   	else if (items[2].textContent == 'o' && items[5].textContent == 'o' && items[8].textContent == 'o'){
 		   	   		cross.classList.add('col3');
-					alert('winner o');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: O');
 		   	   	} 
 		   	   	else if (items[0].textContent == 'o' && items[4].textContent == 'o' && items[8].textContent == 'o'){
 		   	   		cross.classList.add('diag1');
-					alert('winner o');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: O');
 		   	   	} 
 		   	   	else if (items[2].textContent == 'o' && items[4].textContent == 'o' && items[6].textContent == 'o'){
 					cross.classList.add('diag2');
-					alert('winner o');
-				} 
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: O');
+				}
 		}
 		else {
 			turns++;
@@ -80,39 +89,75 @@ items.forEach( (e) => {
 			e.classList.add('disable', 'black');
 			if  (items[0].textContent == 'x' && items[1].textContent == 'x' && items[2].textContent == 'x') {
 					cross.classList.add('row1');
-					alert('winner x');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: X');
 			} 
 				else if (items[3].textContent == 'x' && items[4].textContent == 'x' && items[5].textContent == 'x'){
 					cross.classList.add('row2');
-					alert('winner x');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: X');
 				} 
 		   		else if (items[6].textContent == 'x' && items[7].textContent == 'x' && items[8].textContent == 'x'){
 		   			cross.classList.add('row3');
-					alert('winner x');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: X');
 		   		} 
 		   		else if (items[0].textContent == 'x' && items[3].textContent == 'x' && items[6].textContent == 'x'){
 		   			cross.classList.add('col1');
-					alert('winner x');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: X');
 		   		} 
 		   		else if (items[1].textContent == 'x' && items[4].textContent == 'x' && items[7].textContent == 'x'){
 		   			cross.classList.add('col2');
-					alert('winner x');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: X');
 		   		} 
 		   		else if (items[2].textContent == 'x' && items[5].textContent == 'x' && items[8].textContent == 'x'){
 		   			cross.classList.add('col3');
-					alert('winner x');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: X');
 		   		} 
 		   		else if (items[0].textContent == 'x' && items[4].textContent == 'x' && items[8].textContent == 'x'){
 		   			cross.classList.add('diag1');
-					alert('winner x');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: X');
 		   		} 
 		   		else if (items[2].textContent == 'x' && items[4].textContent == 'x' && items[6].textContent == 'x'){
 					cross.classList.add('diag2');
-					alert('winner x');
+					setTimeout(function () {refresh(items);}, 2000);
+					popupShow ('Winner: X');
 				}
+		}
+
+		if (turns == 9 && !overlay.classList.contains('overlay-show')) {
+			popupShow ('Tie game');
+			setTimeout(function () {refresh(items);}, 2000);
 		}
 	});
 
+});
+
+window.addEventListener('keydown', (e) => {
+	if(e.keyCode === 27){
+		if(overlay.classList.contains('overlay-show')){
+			overlay.classList.remove('overlay-show');
+			popup.classList.remove('popup-show');
+		}
+	}
+});
+
+overlay.addEventListener('click', (e) => {
+	if(overlay.classList.contains('overlay-show')){
+			overlay.classList.remove('overlay-show');
+			popup.classList.remove('popup-show');
+		}
+});
+
+close.addEventListener('click', (e) =>{
+	if(overlay.classList.contains('overlay-show')){
+			overlay.classList.remove('overlay-show');
+			popup.classList.remove('popup-show');
+		}
 });
 
 function createItem (text, className) {
@@ -130,3 +175,13 @@ function refresh (arr) {
 		elem.classList.remove('disable', 'red', 'black');
 	});
 }
+
+function popupShow (text) {
+	overlay.classList.add('overlay-show');
+	popup.classList.add('popup-show');
+	head.textContent = text;
+}
+
+
+
+
